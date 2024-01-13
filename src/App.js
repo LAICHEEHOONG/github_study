0eee726e4e49ffc7ae202c77ff44d9ce5439c98a
LAI CHEE HOONG <47762131+LAICHEEHOONG@users.noreply.github.com>
2024-01-13 11:21:15 +0800

    add branch name to test-1 branch
    add branch name to test-1 branch

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Branch test-1
+        </a> */}
+        <h1 className="App-link">Branch test-1</h1>
       </header>
     </div>
   );
